65b313e9de6b01faf848fcb3b2990e8936a13b24
marcpineda888 <77488025+marcpineda888@users.noreply.github.com>
2021-01-15 11:33:25 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Non")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("OUI")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Non")
+})
+basic.showString("Salut")
